34d30d799636314ec91ac65cc4a458828cc02665
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-08-03 19:47:23 +0900

    chatGPT API 제거, txt 파일로 가공한 데이터를 활용하도록 변경
    한국어 문장, 한국어 단어, 영어 문장, 영어 단어를 각각의 txt 파일로 저장.
    무작위로 선정한 한국어 단어, 한국어 문장을 제공하면 사용자는 영어 단어, 영어 문장을 입력.
    입력이 끝나면 정답 여부를 출력.
    특정 명령어를 사용하면 문제를 넘기거나, 프로그램을 종료할 수 있음.

--- main.py
+++ main.py
@@ -1,36 +1,75 @@
-import os
-import openai
+## main.py
+import random
 
-## openAI API Key
-openai.api_key = "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"
+def answer_check_word(answer_words, words_en, random_index) :
+    if (answer_words == words_en[random_index]) :
+        return "Correct!"
+    else :
+        return words_en[random_index]
+
+def answer_check_sentence(answer_sentences, sentences_en, random_index) :
+    if (sentences_en[random_index] == answer_sentences) :
+        return "Correct!"
+    else :
+        return sentences_en[random_index]
 
 def main() :
-    print("Processing...")
+    with open("Unit 1/Words_EN.txt") as words :     ## 영단어
+        words_en = words.readlines()
 
-    ## txt file Load
-    with open("Txt/Words.txt") as f :
-        words = f.readlines()
-    words = [i.strip() for i in words]
+    with open("Unit 1/Words_KOR.txt") as words :    ## 한국어 단어
+        words_kor = words.readlines()
 
-    with open("Txt/System.txt", 'r') as f :
-        sys_content = f.read()
+    with open("Unit 1/Sentences_EN.txt") as sentences :    ## 영문장
+        sentences_en = sentences.readlines()
 
-    request = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {
-                "role" : "system",
-                "content" : sys_content
-            }
-        ],
-        temperature = 1,
-        max_tokens = 256,
-        top_p = 1,
-        frequency_penalty = 0,
-        presence_penalty = 0
-    )
+    with open("Unit 1/Sentences_KOR.txt") as sentences :   ## 한국어 문장
+        sentences_kor = sentences.readlines()
 
-    print(request.choices[0].message.content)
+
+    words_en = [i.strip() for i in words_en]
+    words_kor = [i.strip() for i in words_kor]
+
+    sentences_en = [i.strip() for i in sentences_en]
+    sentences_kor = [i.strip() for i in sentences_kor]
+
+    while (True) :
+        random_index = random.randrange(0, len(words_en))
+
+        question_words = words_kor[random_index]
+        question_sentences = sentences_kor[random_index]
+
+        print("- Please answer the question.")
+
+        print("Q1: " + question_words)
+        answer_words = input("A1: ")
+
+        if (answer_words == "/exit") :
+            break
+        elif (answer_words == "/pass") :
+            print(words_en[random_index])
+            print()
+
+            continue
+
+        print("Q2: " + question_sentences)
+        answer_sentences = input("A2: ")
+
+        if (answer_sentences == "/exit") :
+            break
+        elif (answer_sentences == "/pass") :
+            print(words_en[random_index])
+            print(sentences_en[random_index])
+            print()
+
+            continue
+
+        print("- Result")
+        print(answer_check_word(answer_words, words_en, random_index))
+        print(answer_check_sentence(answer_sentences, sentences_en, random_index))
+        print()
+
+
 
 if (__name__ == "__main__") :
     main()
